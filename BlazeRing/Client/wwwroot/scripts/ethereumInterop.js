3a740d0e0defdfcbc0b6de7689d36fc60b5141f5
Fudgey <fudgebucket27@gmail.com>
2023-06-08 18:04:22 +1000

    metamask connection working

--- BlazeRing/Client/wwwroot/scripts/ethereumInterop.js
+++ BlazeRing/Client/wwwroot/scripts/ethereumInterop.js
@@ -1,5 +1,10 @@
 ﻿window.ethereumInterop = {
     getAccount: function () {
+        if (typeof ethereum === 'undefined') {
+            console.error('Ethereum provider not found');
+            return;
+        }
+
         return ethereum.request({ method: 'eth_requestAccounts' })
             .then(accounts => accounts[0])
             .catch(error => console.error(error));
