5e7bcf1b36d9c7c55abca12ea3d11194ccc52692
Fudgey <fudgebucket27@gmail.com>
2023-06-08 22:15:51 +1000

    speed boost!

---
+++ BlazeRing/Server/Controllers/SigningController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PoseidonSharp;
+using System.Numerics;
+
+namespace BlazeRing.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SigningController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("Sign")]
+        public async Task<string> GetApiKeyEDDSASig(string signedEcdsaMessage, string address, int accountId)
+        {
+            var _keyDeets = await Task.Run(() => PoseidonHelper.GetL2PKFromMetaMask(signedEcdsaMessage, address));
+            string _api_signatureBase = "GET&https%3A%2F%2Fapi3.loopring.io%2Fapi%2Fv3%2FapiKey&accountId%3D" + accountId;
+            BigInteger _sigbaseToBitInt = SHA256Helper.CalculateSHA256HashNumber(_api_signatureBase);
+            Eddsa eddsa = new Eddsa(_sigbaseToBitInt, _keyDeets.secretKey); //Put in the calculated poseidon hash in order to Sign
+            string signedMessage = eddsa.Sign();
+            return await Task.Run(() => eddsa.Sign());
+        }
+    }
+}
