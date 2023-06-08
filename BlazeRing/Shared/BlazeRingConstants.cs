7786e92846e1ec409912e1c455ad0c887b15f412
John <fudgebucket27@gmail.com>
2023-06-08 21:31:20 +1000

    get api key is working!

--- BlazeRing/Shared/BlazeRingConstants.cs
+++ BlazeRing/Shared/BlazeRingConstants.cs
@@ -9,6 +9,7 @@ namespace BlazeRing.Shared
     public static class BlazeRingConstants
     {
         public static string ApiUrl = "https://api3.loopring.io";
-        public static string AccountInformationApiEndpoint = "/api/v3/account";
+        public static string GetAccountInformationApiEndpoint = "/api/v3/account";
+        public static string GetApiKeyEndpoint = "api/v3/apiKey";
     }
 }
