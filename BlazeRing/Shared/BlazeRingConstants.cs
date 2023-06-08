bfe9f93590cdc14668569b3b83ee0b58383fcc1b
John <fudgebucket27@gmail.com>
2023-06-08 20:27:41 +1000

    connect working

---
+++ BlazeRing/Shared/BlazeRingConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazeRing.Shared
+{
+    public static class BlazeRingConstants
+    {
+        public static string ApiUrl = "https://api3.loopring.io";
+        public static string AccountInformationApiEndpoint = "/api/v3/account";
+    }
+}
