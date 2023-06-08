1cad86ca47f8b2fced9354cbf93d019eaa296973
Fudgey <fudgebucket27@gmail.com>
2023-06-08 15:42:37 +1000

    boiler plate

---
+++ BlazeRing/Client/wwwroot/scripts/ethereum.js
@@ -0,0 +1,7 @@
+﻿export function connect() {
+    if (window.ethereum) {
+        window.ethereum.request({ method: 'eth_requestAccounts' });
+    } else {
+        console.log('Non-Ethereum browser detected. Consider installing MetaMask.');
+    }
+}
