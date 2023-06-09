cd77c8c7085a112d31a8c0a936b6fc1690e90a32
Fudgey <fudgebucket27@gmail.com>
2023-06-09 10:50:39 +1000

    remove duplicated sign code. more meaningful error messages

--- BlazeRing/Shared/BlazeRingHelper.cs
+++ BlazeRing/Shared/BlazeRingHelper.cs
@@ -16,7 +16,6 @@ namespace BlazeRing.Shared
             string _api_signatureBase = "GET&https%3A%2F%2Fapi3.loopring.io%2Fapi%2Fv3%2FapiKey&accountId%3D" + accountId;
             BigInteger _sigbaseToBitInt = SHA256Helper.CalculateSHA256HashNumber(_api_signatureBase);
             Eddsa eddsa = new Eddsa(_sigbaseToBitInt, _keyDeets.secretKey); //Put in the calculated poseidon hash in order to Sign
-            string signedMessage = eddsa.Sign();
             return await Task.Run(() => eddsa.Sign());
         }
     }
