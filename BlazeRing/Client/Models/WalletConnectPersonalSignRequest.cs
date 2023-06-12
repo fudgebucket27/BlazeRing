c6066b0df243311f2b748e8256190bec83d8acee
Fudgey <fudgebucket27@gmail.com>
2023-06-12 17:00:24 +1000

    boiler plate

---
+++ BlazeRing/Client/Models/WalletConnectPersonalSignRequest.cs
@@ -0,0 +1,13 @@
+﻿using WalletConnectSharp.Network.Models;
+
+namespace BlazeRing.Client.Models
+{
+    [RpcMethod("personal_sign")]
+    public class WalletConnectPersonalSignRequest
+    {
+        public string message { get; set; }
+        public  WalletConnectPersonalSignRequest(string message)
+        {
+            this.message = message;
+        }
+}
