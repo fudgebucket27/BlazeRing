c0a4733b9ba751ef8a7a29eb940a5b985919330c
Fudgey <fudgebucket27@gmail.com>
2023-08-30 10:11:05 +1000

    update poseidon and use helper method

--- BlazeRing/Shared/BlazeRingHelper.cs
+++ BlazeRing/Shared/BlazeRingHelper.cs
@@ -10,12 +10,12 @@ namespace BlazeRing.Shared
 {
     public static class BlazeRingHelper
     {
-        public static async Task<string> GetApiKeyEDDSASig(string signedEcdsaMessage, string address, int accountId)
+        public static async Task<string> GenerateXApiSigHeader(string signedEcdsaMessage, string address, int accountId)
         {
-            var _keyDeets = await Task.Run(() => PoseidonHelper.GetL2PKFromMetaMask(signedEcdsaMessage, address));
-            string _api_signatureBase = "GET&https%3A%2F%2Fapi3.loopring.io%2Fapi%2Fv3%2FapiKey&accountId%3D" + accountId;
-            BigInteger _sigbaseToBitInt = SHA256Helper.CalculateSHA256HashNumber(_api_signatureBase);
-            Eddsa eddsa = new Eddsa(_sigbaseToBitInt, _keyDeets.secretKey); //Put in the calculated poseidon hash in order to Sign
+            var keyDetails = await Task.Run(() => LoopringL2KeyGenerator.GenerateL2KeyDetails(signedEcdsaMessage, address, true));
+            string signatureBase = "GET&https%3A%2F%2Fapi3.loopring.io%2Fapi%2Fv3%2FapiKey&accountId%3D" + accountId;
+            BigInteger signatureBigInt = SHA256Helper.CalculateSHA256HashNumber(signatureBase);
+            Eddsa eddsa = new Eddsa(signatureBigInt, keyDetails.secretKey);
             return await Task.Run(() => eddsa.Sign());
         }
     }
