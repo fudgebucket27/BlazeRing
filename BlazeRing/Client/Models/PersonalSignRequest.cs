079e06c96bb25139c07db2566695e29343f4b435
Fudgey <fudgebucket27@gmail.com>
2023-08-30 11:49:26 +1000

    personal sign mesage working. need to figure out the message to send

---
+++ BlazeRing/Client/Models/PersonalSignRequest.cs
@@ -0,0 +1,12 @@
+﻿using WalletConnectSharp.Common.Utils;
+using WalletConnectSharp.Network.Models;
+
+namespace BlazeRing.Client.Models
+{
+    [RpcMethod("personal_sign"), RpcRequestOptions(Clock.ONE_MINUTE, 99999)]
+    public class PersonalSign : List<string>
+    {
+        public PersonalSign(params string[] personalSignParams)
+            : base(personalSignParams) { }
+    }
+}
